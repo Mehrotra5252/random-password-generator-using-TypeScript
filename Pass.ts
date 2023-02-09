10d014d60405a1b4f5ac8a9c397634369c5c7c34
ਅਕਸ਼ਤ ਮੇਹਰੋਤਰਾ <121358988+Mehrotra5252@users.noreply.github.com>
2023-02-09 23:39:57 +0530

    Create Pass.ts

---
+++ Pass.ts
@@ -0,0 +1,10 @@
+unction generatePassword(length: number): string {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  return password;
+}
+
+console.log(generatePassword(35));
